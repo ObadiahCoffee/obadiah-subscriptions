fa65acc98fac320173eaf3da4ab92446d1f1ff51
samlogan <sam@mammoth.tech>
2021-04-12 05:50:39 +1000

    Add 404 page SCSS modules

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { Link } from 'gatsby';
 import { Layout } from 'components';
 import FourOhFourImage from 'images/404.svg';
-import './404.scss';
+import { container, image, content } from './404.module.scss';
 
 const FourOhFour = ({ location }) => (
   <Layout location={location}>
-    <div className="container not-found-wrap">
-      <img className="not-found-image" src={FourOhFourImage} alt="Opps this page has gone missing" />
-      <div className="not-found-text">
+    <div className={`container ${container}`}>
+      <img className={image} src={FourOhFourImage} alt="Opps this page has gone missing" />
+      <div className={content}>
         <h2>404 Not found</h2>
         <Link className="button" to="/">
           Go back home
